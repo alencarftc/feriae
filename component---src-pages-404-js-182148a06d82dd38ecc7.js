398f1268b0e659f47ffd40ec191ca498e9784e58
Felipe Alencar <alencar.dev@outlook.com>
2020-02-05 23:30:01 -0400

    Updates

--- component---src-pages-404-js-182148a06d82dd38ecc7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),c=t("8k0H"),l=t("vrFN");n.default=function(){return o.a.createElement(c.a,null,o.a.createElement(l.a,{title:"Não encontrado"}),o.a.createElement("h1",null,"Página não encontrada"),o.a.createElement("p",null,"Poxa... Você encontrou uma página inexistente. :("))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-182148a06d82dd38ecc7.js.map
